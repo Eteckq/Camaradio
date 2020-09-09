14d45586d0ea0ea304fe36d1eb6d200ba49ec788
TomDolidon <dolidon.tom@gmail.com>
2020-09-09 19:37:39 +0200

    adding search for music and table

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,6 +1,24 @@
-$("#search").click(function () {
+$("#search").click(async function () {
   const searchValue = $("#searchInput").val();
-  console.log(searchValue);
-  const result = search(searchValue);
-  console.log(result);
+  const result = await search(searchValue);
+  displaySearchResult(result.tracks.items);
 });
+
+function displaySearchResult(searchResults) {
+  $("#searchResultTable").empty();
+
+  searchResults.forEach((searchResult) => {
+    $("#searchResultTable").append(`
+      <tr>
+        <th scope='row'>
+          <button type="button" class="btn btn-dark">Ajouter</button>
+        </th>
+        <td>${searchResult.name}</td>
+        <td>${searchResult.artists[0].name}</td>
+      </tr>`);
+  });
+}
+
+function displayQueueTable() {
+  $("#queueTable").empty();
+}
