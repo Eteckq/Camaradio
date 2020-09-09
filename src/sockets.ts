5ee00914f197247cf0f542cbd7e4f53d632a3fec
Yohan <ygastoud@altalis.fr>
2020-09-09 19:30:02 +0200

    add queue gestion (back)

--- src/sockets.ts
+++ src/sockets.ts
@@ -7,14 +7,16 @@ const queue = new Queue()
 export default function initSockets(io: Server){
     io.on('connection', (client: Socket) => {
         console.log('new client');
+        let userSpotifyId: string;
 
         client.on('hello', data => {
-            console.log('hello', data);
-
+            userSpotifyId = data.id
         })
 
         client.on('addTrack', data => {
-            queue.addTrack(data.trackId, data.userId)
+            queue.addTrack(data.trackId, userSpotifyId)
+
+            io.emit('updateTrackList', queue.getTracksId())
         })
 
         client.on('disconnect', () => {
