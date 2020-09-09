05bf54b6d9ea4d26c111c2c7e630d392f1b515ed
Yohan <ygastoud@altalis.fr>
2020-09-09 19:38:01 +0200

    remove spotify api from back

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import AuthRouter from './Auth';
-import SpotifyRouter from './Spotify';
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
 router.use('/auth', AuthRouter);
-router.use('/spotify', SpotifyRouter);
 
 // Export the base-router
 export default router;
