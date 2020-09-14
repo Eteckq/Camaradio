37ddf2b5b09ea500cc40fac808f66664ede07242
Yohan <ygastoud@altalis.fr>
2020-09-14 16:18:37 +0200

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 export default {
     client_id: 'b37d57048fef48e7a7ff9427ed54f5d8',
     client_secret: '953a6204a29d4f5196c9ed911b37e916',
-    redirect_uri: 'https://dashboard.heroku.com/api/auth/callback',
+    redirect_uri: 'https://camaradio.heroku.com/api/auth/callback',
 }
